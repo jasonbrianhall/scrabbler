47bd99041a3c5d4fe2b5c403e684920c2071622b
Jason Hall <jbhall@fbi.gov>
2019-07-30 10:10:29 -0400

    Scrabbler

---
+++ scrabbler.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+	letters=[]
+	dictionary=[]
+	if len(sys.argv)>2:
+		currentstring=sys.argv[1].lower()
+
+		for x in sys.argv[1]:
+			letters.append(x)
+		# Length of the word being tested
+		wordlength=int(sys.argv[2])
+	else:
+		print "Not enough arguments; requires characters and word length"
+		exit(1)
+	try:
+		f = open("linux.words", "r")
+	except:
+		print "The file linux.words is not found; it's usually found in /usr/share/dict/linux.words; copy it into the currect directory"
+		exit(1)
+	for x in f:
+		# String out Extra Spaces and New Lines
+		temp=x.rstrip()
+		# Don't add anything to a dictonary that is too short or too long
+		if len(temp)==wordlength:
+			dictionary.append(temp)
+	f.close()
+
+	for word in dictionary:
+		# Make case insensitive
+		temp=word.lower()
+		passed=1
+		for x in temp:
+			if not x in currentstring:
+				passed=0
+				break
+		if passed==1:
+			for x in temp:
+				counter=temp.count(x)
+				counter2=currentstring.count(x)
+				if not counter<=counter2:
+					passed=0
+					break
+			if passed==1:
+				print word
+											
+				
+	
+
+if __name__ == "__main__":
+	main()
+
